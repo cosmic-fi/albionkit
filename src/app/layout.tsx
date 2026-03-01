461a81e208890d6bc872c67cdc038db21a128742
cosmic-fi <killrbid@gmail.com>
2026-03-01 10:10:44 +0300

    feat: add Clarity tracking script to RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,6 +115,13 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <script
+          dangerouslySetInnerHTML={{ __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vou0rm1qq5");` }}
+        />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <AuthProvider>
             <LoginModalProvider>
